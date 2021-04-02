652d6516fc6e88365fba66458f69cb79305feae4
dahai-f <1119369173@qq.com>
2021-04-02 22:20:07 +0800

    hot reload

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 #![feature(once_cell)]
 
+use std::collections::{HashMap, LinkedList};
 use std::collections::hash_map::Entry;
-use std::collections::HashMap;
 use std::ffi::{OsStr, OsString};
-use std::lazy::SyncLazy;
 use std::path::{Path, PathBuf};
-use std::sync::mpsc::{channel, Receiver};
+use std::sync::mpsc::{channel, Receiver, TryRecvError};
 use std::time::Duration;
 
 use ::error_chain::*;
@@ -27,23 +26,24 @@ pub struct Loader<D, R: ResultExt<()>> {
     name_to_origin_path: HashMap<String, PathBuf>,
     post_load: fn(&mut D, &Lib) -> R,
     pre_unload: fn(&mut D, &Lib) -> R,
-    search_paths: Vec<PathBuf>,
+    search_dirs: Vec<PathBuf>,
+    pending_remove: LinkedList<PathBuf>,
 }
 
 impl<D, R: ResultExt<()>> Loader<D, R> {
     pub fn new(
-        additional_search_paths: Vec<PathBuf>,
+        additional_search_dirs: Vec<PathBuf>,
         post_load: fn(&mut D, &Lib) -> R,
         pre_unload: fn(&mut D, &Lib) -> R,
     ) -> Result<Self> {
-        let mut search_paths = additional_search_paths;
+        let mut search_dirs = additional_search_dirs;
         let mut exe_dir = std::env::current_exe().chain_err(|| "Failed to get current_exe")?;
         exe_dir.pop();
         #[cfg(test)]
-        {
-            exe_dir.pop();
-        }
-        search_paths.push(exe_dir);
+            {
+                exe_dir.pop();
+            }
+        search_dirs.push(exe_dir);
 
         let (sender, receiver) = channel();
         Ok(Self {
@@ -54,41 +54,104 @@ impl<D, R: ResultExt<()>> Loader<D, R> {
             name_to_origin_path: Default::default(),
             post_load,
             pre_unload,
-            search_paths,
+            search_dirs,
+            pending_remove: Default::default(),
         })
     }
 
     pub fn add_library(&mut self, lib_name: &str, owner_data: &mut D) -> Result<()> {
-        match self.name_to_origin_path.entry(lib_name.to_owned()) {
-            Entry::Occupied(_occupied) => Ok(()),
+        let (origin_path, load_path) = match self.name_to_origin_path.entry(lib_name.to_owned()) {
+            Entry::Occupied(_occupied) => return Ok(()),
             Entry::Vacant(vacant_origin_path) => {
-                let (origin_path, load_path) = Self::search(&self.search_paths, lib_name)
+                let (origin_path, load_path) = Self::search(&self.search_dirs, lib_name)
                     .chain_err(|| format!("Library not exists. name: {}", lib_name))?;
-
-                Self::copy(&origin_path, &load_path)?;
-
-                let lib = unsafe { Library::new(&load_path) }
-                    .chain_err(|| format!("Failed to load library. path: {:?}", load_path))?;
-                self.file_watcher
-                    .watch(&origin_path, RecursiveMode::NonRecursive)
-                    .chain_err(|| "Failed to add watch")?;
-                let lib = Lib {
-                    lib,
-                    name: lib_name.to_owned(),
-                    load_path,
-                    origin_path,
-                };
-                let origin_path = vacant_origin_path.insert(lib.origin_path.clone());
-                self.origin_path_to_lib.insert(origin_path.clone(), lib);
-                (self.post_load)(owner_data, &self.origin_path_to_lib[origin_path])
-                    .chain_err(|| ErrorKind::PostLoadError)
+                (vacant_origin_path.insert(origin_path).clone(), load_path)
             }
-        }
+        };
+        self.add(
+            lib_name.to_owned(),
+            origin_path.clone(),
+            load_path,
+            owner_data,
+        )?;
+        self.file_watcher
+            .watch(&origin_path, RecursiveMode::NonRecursive)
+            .chain_err(|| format!("Failed to watch file. path: {:?}", origin_path))
     }
 
-    pub fn remove_library(&mut self, _name: &str) {}
+    pub fn remove_library(&mut self, lib_name: &str, owner_data: &mut D) -> Result<()> {
+        if let Some(origin_path) = self.name_to_origin_path.remove(lib_name) {
+            self.file_watcher
+                .unwatch(&origin_path)
+                .chain_err(|| format!("Failed to unwatch file. path: {:?}", origin_path))?;
+            self.remove(&origin_path, owner_data)?;
+        }
 
-    pub fn update(&self, _owner_data: &mut D) {}
+        Ok(())
+    }
+
+    pub fn update(&mut self, owner_data: &mut D) -> Result<()> {
+        while let Some(to_remove) = self.pending_remove.front() {
+            if std::fs::remove_file(&to_remove).is_err() {
+                break;
+            } else {
+                self.pending_remove.pop_front();
+            };
+        }
+
+        loop {
+            let event = self.file_receiver.try_recv();
+            match event {
+                Ok(event) => match event {
+                    DebouncedEvent::NoticeWrite(path)
+                    | DebouncedEvent::Create(path)
+                    | DebouncedEvent::Write(path) => {
+                        if let Some(error) = self.remove(&path, owner_data).err() {
+                            eprint!(
+                                "Failed to remove library. origin_path: {:?}, error: {}",
+                                path,
+                                error.display_chain()
+                            )
+                        }
+
+                        let mut load_path = path.clone();
+                        let file_name: &str = load_path
+                            .file_name()
+                            .and_then(|file_name| file_name.to_str())
+                            .chain_err(|| {
+                                format!("Failed to get filename. path: {:?}", load_path)
+                            })?;
+                        let lib_name = utils::extract_lib_name(file_name).chain_err(|| {
+                            format!("Failed to extract lib_name. file_name: {}", file_name)
+                        })?;
+                        let live_file_name = utils::get_load_path(&OsString::from(&lib_name));
+                        load_path.set_file_name(live_file_name);
+                        self.add(lib_name, path, load_path, owner_data)?;
+                    }
+
+                    DebouncedEvent::NoticeRemove(path)
+                    | DebouncedEvent::Remove(path)
+                    | DebouncedEvent::Rename(path, _) => {
+                        self.remove(&path, owner_data)?;
+                    }
+
+                    DebouncedEvent::Chmod(_)
+                    | DebouncedEvent::Rescan
+                    | DebouncedEvent::Error(_, _) => {}
+                },
+                Err(error) => match error {
+                    TryRecvError::Empty => {
+                        break;
+                    }
+                    TryRecvError::Disconnected => {
+                        bail!("File watcher disconnected")
+                    }
+                },
+            }
+        }
+
+        Ok(())
+    }
 
     fn copy(origin: &Path, load: &Path) -> Result<()> {
         std::fs::copy(origin, load)
@@ -102,17 +165,45 @@ impl<D, R: ResultExt<()>> Loader<D, R> {
         for dir in search_dirs {
             let origin_path = dir.join(&file_name);
             if origin_path.exists() {
-                static LIVE_SUFFIX: SyncLazy<&OsStr> = SyncLazy::new(|| "_live".as_ref());
-                let live_suffix = *LIVE_SUFFIX;
-                let mut live_file_name =
-                    OsString::with_capacity(lib_name.len() + live_suffix.len());
-                live_file_name.push(lib_name);
-                live_file_name.push(live_suffix);
-                let load_path = dir.join(libloading::library_filename(live_file_name));
+                let live_lib_name = utils::get_load_path(dir, lib_name);
+                let load_path = ;
                 return Some((origin_path, load_path));
             }
         }
         None
+    }
+
+    fn add(
+        &mut self,
+        lib_name: String,
+        origin_path: PathBuf,
+        load_path: PathBuf,
+        owner_data: &mut D,
+    ) -> Result<()> {
+        Self::copy(&origin_path, &load_path)?;
+
+        let lib = unsafe { Library::new(&load_path) }
+            .chain_err(|| format!("Failed to load library. path: {:?}", load_path))?;
+        let lib = Lib {
+            lib,
+            name: lib_name,
+            load_path,
+            origin_path,
+        };
+        (self.post_load)(owner_data, &lib).chain_err(|| ErrorKind::PostLoadError)?;
+        self.origin_path_to_lib.insert(lib.origin_path.clone(), lib);
+        Ok(())
+    }
+    fn remove(&mut self, origin_path: &Path, owner_data: &mut D) -> Result<()> {
+        let lib = self
+            .origin_path_to_lib
+            .remove(origin_path)
+            .chain_err(|| format!("Failed to find lib. origin_path: {:?}", origin_path))?;
+        (self.pre_unload)(owner_data, &lib).chain_err(|| ErrorKind::PreUnloadError)?;
+        let load_path = lib.load_path.clone();
+        drop(lib);
+        self.pending_remove.push_back(load_path);
+        Ok(())
     }
 }
 
@@ -129,5 +220,62 @@ impl Lib {
     }
     pub fn lib(&self) -> &Library {
         &self.lib
+    }
+}
+
+mod utils {
+    use std::ffi::{OsStr, OsString};
+    use std::path::PathBuf;
+
+    use ::error_chain::error_chain;
+    use lazy_static::lazy_static;
+    use regex::Regex;
+
+    error_chain! {}
+
+    pub(crate) fn get_load_path(dir: PathBuf, lib_name: &OsStr) -> OsString {
+        dir.join(libloading::library_filename(live_lib_name))
+        let live_suffix: &OsStr = "_live".as_ref();
+        let mut live_file_name = OsString::with_capacity(lib_name.len() + live_suffix.len());
+        live_file_name.push(lib_name);
+        live_file_name.push(live_suffix);
+        live_file_name
+    }
+
+    pub(crate) fn extract_lib_name(file_name: &str) -> Result<String> {
+        lazy_static! {
+            static ref REGEX: Regex = Regex::new(&format!(
+                r"^{}(.+){}$",
+                std::env::consts::DLL_PREFIX,
+                std::env::consts::DLL_SUFFIX,
+            ))
+            .unwrap();
+        }
+
+        REGEX
+            .captures(file_name)
+            .and_then(|cap| cap.get(1))
+            .map(|ma| ma.as_str().to_owned())
+            .chain_err(|| format!("Failed to get lib_name. file_name: {:?}", file_name))
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn extract_lib_name() {
+            let file_name = format!(
+                "{}gl32{}",
+                std::env::consts::DLL_PREFIX,
+                std::env::consts::DLL_SUFFIX
+            );
+            assert_eq!(crate::utils::extract_lib_name(&file_name).unwrap(), "gl32");
+
+            let file_name = format!(
+                "{}gl32{}a",
+                std::env::consts::DLL_PREFIX,
+                std::env::consts::DLL_SUFFIX
+            );
+            assert!(crate::utils::extract_lib_name(&file_name).is_err());
+        }
     }
 }
